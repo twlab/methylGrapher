8db6e6df11a0b1cd4b4185a7391dd03fa711a7f7
Hendricks27 <wenjinzhang95@gmail.com>
2024-03-13 13:21:40 -0500

    example update

--- example/pipeline.sh
+++ example/pipeline.sh
@@ -16,7 +16,7 @@ python3 ../src/main.py PrepareGenome -gfa $input_gfa -lp ./lambda.fa -prefix $ge
 
 
 # 2. Run MethylGrapher with one-line command
-python3 ../src/main.py simple -fq1 ./R1.fastq -fq2 ./R2.fastq -index_prefixv $genome_prefix -work_dir $work_dir -directional Y
+python3 ../src/main.py main -fq1 ./R1.fastq -fq2 ./R2.fastq -index_prefixv $genome_prefix -work_dir $work_dir -directional Y
 
 
 # (OPTIONAL) sorting output
